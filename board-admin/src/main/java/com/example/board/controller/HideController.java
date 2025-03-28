572f1bb29da979df897efa5a6812f732422198b1
gangsterAnt <francis@jeong-gyutaeg-ui-MacBookAir.local>
2025-03-28 22:29:41 +0900

    [3/28] admin 구현 - 보이기 숨기기 결과페이지 생성

--- board-admin/src/main/java/com/example/board/controller/HideController.java
+++ board-admin/src/main/java/com/example/board/controller/HideController.java
@@ -16,32 +16,32 @@ public class HideController {
     @PostMapping("/hide/post")
     public String hidePost(@RequestParam Long id) {
         if (hideService.hidePost(id)) {
-            return "hide post complete. post id : " + id;
+            return "admin/hide-success";
         }
-        return "hide fail. Please check warning log.";
+        return "admin/hide-fail";
     }
 
     @PostMapping("/un-hide/post")
     public String unHidePost(@RequestParam Long id) {
         if (hideService.unHidePost(id)) {
-            return "un-hide post complete. post id : " + id;
+            return "admin/un-hide-success";
         }
-        return "un-hide fail. Please check warning log.";
+        return "admin/un-hide-fail";
     }
 
     @PostMapping("/hide/comment")
     public String hideComment(@RequestParam Long id) {
         if (hideService.hideComment(id)) {
-            return "hide comment complete. post id : " + id;
+            return "admin/hide-success";
         }
-        return "hide fail. Please check warning log.";
+        return "admin/hide-fail";
     }
 
     @PostMapping("/un-hide/comment")
     public String unHideComment(@RequestParam Long id) {
         if (hideService.unHideComment(id)) {
-            return "un-hide comment complete. post id : " + id;
+            return "admin/un-hide-success";
         }
-        return "un-hide fail. Please check warning log.";
+        return "admin/un-hide-fail";
     }
 }
